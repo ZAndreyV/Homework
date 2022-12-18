0b29fd22c3a02a707f81084b76eb9f3ff9676e25
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2022-12-18 20:46:03 +0700

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startnumber = number * 0 + 2;
+while (number >= startnumber)
+{
+    Console.Write($"{startnumber}, ");
+    startnumber = startnumber + 2;
+}
